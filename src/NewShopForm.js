5754d3e73ac84488d873540479bec49a14c89cc3
shivambalar1710 <146896846+shivambalar1710@users.noreply.github.com>
2023-10-17 21:43:03 -0400

    Update NewShopForm.js

--- src/NewShopForm.js
+++ src/NewShopForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import NewProductForm from './NewProductForm';
 
 function NewShopForm() {
   const [shopData, setShopData] = useState({
@@ -6,6 +7,8 @@ function NewShopForm() {
     description: '',
     logo: null,
   });
+
+  const [currentPage, setCurrentPage] = useState('new-shop');
 
   const handleChange = (e) => {
     const { name, value, files } = e.target;
@@ -15,26 +18,31 @@ function NewShopForm() {
     });
   };
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Form submitted:', shopData);
-
-    const formData = new FormData();
-    formData.append('name', shopData.name);
-    formData.append('description', shopData.description);
-    formData.append('logo', shopData.logo);
-
-    try {
-      const response = await fetch('http://example.com/api/shops', {
-        method: 'POST',
-        body: formData,
-      });
-      const data = await response.json();
-      console.log('Submission successful:', data);
-    } catch (error) {
-      console.error('Submission failed:', error);
+  
+    if (!shopData.name) {
+      alert('Name is required');
+      return;
     }
+    if (!shopData.description) {
+      alert('Description is required');
+      return;
+    }
+    if (!shopData.logo) {
+      alert('Logo is required');
+      return;
+    }
+  
+    console.log('Form submitted:', shopData);
+    setShopData({
+      name: '',
+      description: '',
+      logo: null,
+    });
   };
+  
+  
 
   const handleCancel = () => {
     console.log('Form cancelled');
@@ -46,51 +54,75 @@ function NewShopForm() {
     });
   };
 
+  const handleNavClick = (page) => {
+    setCurrentPage(page);
+  };
+
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>New Shop</h2>
-      
-      <div>
-        <input 
-          type="file" 
-          id="logo" 
-          name="logo"
-          accept="image/*"
-          onChange={handleChange}
-          style={{ display: 'none' }}
-        />
-        <label htmlFor="logo">Upload Logo</label>
-        <button type="button" onClick={() => document.getElementById('logo').click()}>
-          Upload <span role="img" aria-label="upload">⬆️</span>
-        </button>
-      </div>
-      
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input 
-          type="text" 
-          id="name" 
-          name="name"
-          value={shopData.name}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      
-      <div>
-        <label htmlFor="description">Description:</label>
-        <textarea 
-          id="description" 
-          name="description"
-          value={shopData.description}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      
-      <button type="submit">Submit</button>
-      <button type="button" onClick={handleCancel}>Cancel</button>
-    </form>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <a href="#" onClick={() => handleNavClick('new-shop')}>New Shop</a>
+          </li>
+          <li>
+            <a href="#" onClick={() => handleNavClick('new-product')}>New Product</a>
+          </li>
+        </ul>
+      </nav>
+
+      {currentPage === 'new-shop' ? (
+        <form onSubmit={handleSubmit}>
+          
+          <h2>New Shop</h2>
+        
+        <div>
+          <input 
+            type="file" 
+            id="logo" 
+            name="logo"
+            accept="image/*"
+            onChange={handleChange}
+            style={{ display: 'none' }}
+            required
+          />
+          <label htmlFor="logo">Upload Logo</label>
+          <button type="button" onClick={() => document.getElementById('logo').click()}>
+            Upload <span role="img" aria-label="upload">⬆️</span>
+          </button>
+        </div>
+        
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input 
+            type="text" 
+            id="name" 
+            name="name"
+            value={shopData.name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        
+        <div>
+          <label htmlFor="description">Description:</label>
+          <textarea 
+            id="description" 
+            name="description"
+            value={shopData.description}
+            onChange={handleChange}
+            required
+          />
+        </div>
+
+          
+          <button type="submit">Submit</button>
+          <button type="button" onClick={handleCancel}>Cancel</button>
+        </form>
+      ) : (
+        <NewProductForm />
+      )}
+    </div>
   );
 }
 
